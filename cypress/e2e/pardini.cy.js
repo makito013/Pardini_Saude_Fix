06c9fefd48961092a7d00e8ffdc93d3cbc477ba3
Bruno Andrade <bandradest@gmail.com>
2022-10-04 12:18:12 -0300

    fix

--- cypress/e2e/pardini.cy.js
+++ cypress/e2e/pardini.cy.js
@@ -2,22 +2,24 @@ describe('Login e registro', () => {
 
     beforeEach(() => {
         sessionStorage.setItem('D3VL0', '@SNVRaro');
+
+        cy.on('uncaught:exception', (err, runnable) => {
+          // returning false here prevents Cypress from
+          // failing the test
+          return false
+        });
       });
 
     it('login com sucesso', () => {
-        Cypress.on('uncaught:exception', (err, runnable) => {
-            // returning false here prevents Cypress from
-            // failing the test
-            return false
-          });
-
+        
+        console.log('ceguei ate aqui')
         cy.visit("http://hml.saudefix.com.br/");
-        cy.wait(1000);
         //cy.get('[data-testid="devPass"]').type("@SNVRaro");
         //cy.getByTestId('loginLogoutButton');
         //cy.get('[data-cy="loginLogoutButton"]').click();
         cy.contains("Tranquilidade na saúde para você").should("be.visible");
-
+        cy.get('[data-testid="loginLogoutButton"]').click();
+        cy.contains("Acesso com e-mail ou CPF!").should("be.visible");
     });
 
 });
